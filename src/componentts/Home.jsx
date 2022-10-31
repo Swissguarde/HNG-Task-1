b871f0c974a4e51b26c8d3889945342a05dca33b
Kolade Ayomide <ayomikolade2003@gmail.com>
2022-10-31 23:19:32 +0000

    mapped through links and added styles

--- src/componentts/Home.jsx
+++ src/componentts/Home.jsx
@@ -23,6 +23,40 @@ const Home = () => {
       id: "book__design",
     },
   ];
-  return <div></div>;
+  return (
+    <div className="mx-auto flex flex-col items-center justify-center py-[64px] px-[112px]">
+      {" "}
+      <div>
+        {" "}
+        <img
+          src={profilePic}
+          className="h-[88px] w-[88px] rounded-[275px] object-cover"
+          alt="profile_picture"
+          id="profile__img"
+        />{" "}
+      </div>{" "}
+      <h2 className="my-4 text-xl font-bold">Kolade Ayomide</h2>{" "}
+      <a
+        href="https://app.slack.com/client/T042F7V19Q8/C041V6WKAUS/rimeto_profile/U0489MLK4RK"
+        id="slack"
+        className="my-2 hidden h-[76px] w-[1152px] items-center justify-center rounded-lg border border-[#EAECF0] bg-[#EAECF0] text-center"
+      >
+        {" "}
+        Slack Link{" "}
+      </a>{" "}
+      {links.map((link, i) => (
+        <a
+          href={link.to}
+          key={i}
+          id={link.id}
+          target="_blank"
+          className="my-2 flex h-[76px] w-[1152px] items-center justify-center rounded-lg border border-[#EAECF0] bg-[#EAECF0] text-center"
+        >
+          {" "}
+          {link.name}{" "}
+        </a>
+      ))}{" "}
+    </div>
+  );
 };
 export default Home;
