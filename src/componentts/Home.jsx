9ddae664a1b6a4e3afc2a34b704357b225f426af
Kolade Ayomide <ayomikolade2003@gmail.com>
2022-10-31 23:17:48 +0000

    added links array

--- src/componentts/Home.jsx
+++ src/componentts/Home.jsx
@@ -1,4 +1,28 @@
 const Home = () => {
+  const links = [
+    {
+      name: "Twitter Link",
+      to: "https://twitter.com/swiss_45",
+      id: "twitter",
+    },
+    { name: "Zuri Team", to: "https://training.zuri.team/", id: "btn__zuri" },
+    { name: "Zuri Books", to: " http://books.zuri.team", id: "books" },
+    {
+      name: "Python Books",
+      to: "https://books.zuri.team/python-for-beginners?ref_id=ayomikolade2003",
+      id: "book__python",
+    },
+    {
+      name: "Background Check For Coders",
+      to: "https://background.zuri.team",
+      id: "pitch",
+    },
+    {
+      name: "Design Books",
+      to: "https://books.zuri.team/design-rules",
+      id: "book__design",
+    },
+  ];
   return <div></div>;
 };
 export default Home;
